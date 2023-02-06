bc15d0269725a3f432d94c5388c5ffad7fae6406
Hisham Unniyankal <uaj.hisham@gmail.com>
2023-02-06 10:33:01 +0100

    Restructured dataset generation

--- dataset.py
+++ dataset.py
@@ -1,16 +1,54 @@
-
+import random
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
+from torch.utils.data import Sampler
 from torchtext.data.utils import get_tokenizer
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split
 from torchtext.vocab import build_vocab_from_iterator
-from batch_sampler import BatchSamplerSimilarLength
 from torch.utils.data import DataLoader
 
 
 tokenizer = get_tokenizer("basic_english")
+
+class BatchSamplerSimilarLength(Sampler):
+    def __init__(self, dataset, batch_size,tokenizer ,indices=None, shuffle=True):
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        # get the indices and length
+        self.indices = [(i, len(tokenizer(s[0]))) for i, s in enumerate(dataset)]
+        # if indices are passed, then use only the ones passed (for ddp)
+        if indices is not None:
+            self.indices = torch.tensor(self.indices)[indices].tolist()
+    
+    def __iter__(self):
+        if self.shuffle:
+            random.shuffle(self.indices)
+
+        pooled_indices = []
+        # create pool of indices with similar lengths
+        for i in range(0, len(self.indices), self.batch_size * 100):
+            pooled_indices.extend(sorted(self.indices[i:i + self.batch_size * 100], key=lambda x: x[1]))
+        self.pooled_indices = [x[0] for x in pooled_indices]
+        
+        # Comment in for validation
+        #self.pooled_lengths = [x[1] for x in pooled_indices]
+        #print(self.pooled_lengths)
+        #print(self.pooled_indices)
+
+        # yield indices for current batch
+        batches = [self.pooled_indices[i:i + self.batch_size] for i in
+                   range(0, len(self.pooled_indices), self.batch_size)]
+
+        if self.shuffle:
+            random.shuffle(batches)
+        for batch in batches:
+            yield batch
+
+    def __len__(self):
+        return len(self.pooled_indices) // self.batch_size
+
 
 class Dataset:
     def __init__(self, dataset, config, generator=True):
